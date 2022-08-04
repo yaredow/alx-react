277d1d56efe1ae1cd0edc21c372ff2191ca64ec8
yaredow <yaredyilma11@gmail.com>
2022-08-04 21:47:10 +0300

    task 0

---
+++ 0x06-React_Immutable/0-fromjs.js
@@ -0,0 +1,9 @@
+/*
+function getImmutableObject that accepts object as a parameter and
+converts it into an immutable Map using fromJS of the Immutable.js library
+*/
+import { fromJS } from 'immutable';
+
+export default function getImmutableObject(object) {
+  return fromJS(object);
+}
